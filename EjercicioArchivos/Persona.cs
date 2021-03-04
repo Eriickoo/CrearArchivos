44aed9d1717af71fbfd4562b6d27672b25a19af5
ERICK EMMANUEL JUAREZ MORENO <eriickoo@comunidad.unam.mx>
2021-03-04 16:40:56 -0600

    Primer y ultimo commit

---
+++ EjercicioArchivos/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioArchivos
+{
+    class Contacto
+    {
+        public string nombre { set; get; }
+        public string apellido { set; get; }
+        public string telefono { set; get; }
+        public string correo { set; get; }
+
+        public Contacto()
+        {
+
+        }
+        public Contacto(string nombre, string apellido, string telefono, string correo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        public override string ToString()
+        {
+            return string.Format("Nombre: {0} Apellido: {1} Teléfono: {2} Correo: {3}", nombre, apellido, telefono, correo);
+        }
+    }
+}
---
+++ EjercicioArchivos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var writer = new StreamWriter("directorio.txt"))
+            {
+                
+                string opc = "no";
+                while (String.Compare(opc, "salir", true) != 0)
+                {
+                    Contacto p = new Contacto();
+                    Console.Write("Nombre: ");
+                    p.nombre = Console.ReadLine();
+                    Console.Write("Apellido: ");
+                    p.apellido = Console.ReadLine();
+                    Console.Write("Teléfono: ");
+                    p.telefono = Console.ReadLine();
+                    Console.Write("Correo: ");
+                    p.correo = Console.ReadLine();
+
+                    writer.WriteLine(p.ToString());
+
+                    Console.WriteLine("Escribe salir para salir o cualquier otra cosa para agregar otro contacto");
+                    opc = Console.ReadLine();
+                }
+            }
+        }
+        }
+}
